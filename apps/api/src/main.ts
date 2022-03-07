2afb21b38d0cf9867c6d3cc1d0e983d2dbc81c3c
Sam Vendittelli <sam.vendittelli@sap.com>
2022-03-07 12:14:47 +0000

    ci: add github actions

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import { Message } from '@theme/api-interfaces';
+import { Message } from '@theme-system/api-interfaces';
 
 const app = express();
 
--- apps/theme-e2e/src/integration/app.spec.ts
+++ apps/theme-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('theme', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome theme');
+    getGreeting().contains('Welcome theme-system');
   });
 });
